03fb9a89db5127a2b20ed876fb1fab99226d6b40
Lukáš Kořán <lukas.koran@gmail.com>
2016-03-13 10:23:20 +0100

    Move to Github

---
+++ PdfDocumentChecker/MainForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace PdfDocumentChecker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.wrongBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.fileNameLabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 20);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(15, 16);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(592, 489);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // okBtn
+            // 
+            this.okBtn.AutoSize = true;
+            this.okBtn.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.okBtn.Location = new System.Drawing.Point(0, 0);
+            this.okBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(103, 23);
+            this.okBtn.TabIndex = 1;
+            this.okBtn.Text = "OK";
+            this.okBtn.UseVisualStyleBackColor = false;
+            this.okBtn.Click += new System.EventHandler(this.OkButtonClick);
+            // 
+            // wrongBtn
+            // 
+            this.wrongBtn.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.wrongBtn.Location = new System.Drawing.Point(107, -1);
+            this.wrongBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.wrongBtn.Name = "wrongBtn";
+            this.wrongBtn.Size = new System.Drawing.Size(103, 23);
+            this.wrongBtn.TabIndex = 2;
+            this.wrongBtn.Text = "WRONG";
+            this.wrongBtn.UseVisualStyleBackColor = false;
+            this.wrongBtn.Click += new System.EventHandler(this.WrongButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.wrongBtn);
+            this.panel1.Controls.Add(this.okBtn);
+            this.panel1.Controls.Add(this.fileNameLabel);
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 20, 0, 0);
+            this.panel1.Size = new System.Drawing.Size(592, 509);
+            this.panel1.TabIndex = 3;
+            // 
+            // fileNameLabel
+            // 
+            this.fileNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.fileNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.fileNameLabel.Location = new System.Drawing.Point(234, 0);
+            this.fileNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.fileNameLabel.Name = "fileNameLabel";
+            this.fileNameLabel.Size = new System.Drawing.Size(356, 19);
+            this.fileNameLabel.TabIndex = 3;
+            this.fileNameLabel.Text = "fileName";
+            this.fileNameLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 509);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MainForm";
+            this.Text = "Pdf document checker";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.Button wrongBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label fileNameLabel;
+    }
+}
+
---
+++ PdfDocumentChecker/MainForm.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PdfDocumentChecker
+{
+    public partial class MainForm : Form
+    {
+        private const string OkFolder = "\\OK\\";
+        private const string WrongFolder = "\\WRONG\\";
+        private string _pdfFolder;
+        
+
+        private List<string> _fileList;
+        private List<string>.Enumerator _enumerator;
+        private string _lastFile;
+        private bool _lastClick = true;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            AcceptButton = okBtn;
+            CancelButton = wrongBtn;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string config = Directory.GetCurrentDirectory() + "\\config.txt";
+            if (!File.Exists(config))
+            {
+                using (FolderBrowserDialog dlg = new FolderBrowserDialog())
+                {
+                    dlg.Description = "Choose PDF folder";
+                    if (dlg.ShowDialog() == DialogResult.OK)
+                    {
+                        // MessageBox.Show("You selected: " + dlg.SelectedPath);
+                        StreamWriter file = new StreamWriter(config);
+                        file.WriteLine(dlg.SelectedPath);
+                        file.Close();
+                        _pdfFolder = dlg.SelectedPath;
+                    }
+                    else
+                    {
+                        MessageBox.Show("You didn't chooose any folder", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        Environment.Exit(1);
+                    }
+                }
+            }
+            else
+            {
+                StreamReader file = new StreamReader(config);
+                _pdfFolder = file.ReadLine();
+                file.Close();
+            }
+            bool exists = Directory.Exists(_pdfFolder + OkFolder);
+            if (!exists)
+            {
+                Directory.CreateDirectory(_pdfFolder + OkFolder);
+                Directory.CreateDirectory(_pdfFolder + WrongFolder);
+            }
+            _fileList = new List<string>(Directory.GetFiles(_pdfFolder, "*.pdf", SearchOption.TopDirectoryOnly));
+            _enumerator = _fileList.GetEnumerator();
+            NextFile();
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            MoveFile();
+            okBtn.Focus();
+        }
+
+        private void MoveFile()
+        {
+            if (_lastFile != null)
+            {
+                string dir = Path.GetDirectoryName(_lastFile);
+                string fn = Path.GetFileName(_lastFile);
+                string path = dir + OkFolder + fn;
+                string clickDir = _lastClick ? OkFolder : WrongFolder;
+                File.Move(_lastFile, dir + clickDir + fn);
+            }
+            
+        }
+
+
+        private void OkButtonClick(object sender, EventArgs e)
+        {
+            _lastClick = true;
+            NextFile();
+        }
+
+        private void WrongButton_Click(object sender, EventArgs e)
+        {
+            _lastClick = false;
+            NextFile();
+        }
+
+        private void NextFile()
+        {
+            _lastFile = _enumerator.Current;
+            if (_enumerator.MoveNext())
+            {
+                string currentFileName = _enumerator.Current;
+                webBrowser1.Navigate(@"file:///" + currentFileName);
+                string fn = Path.GetFileName(currentFileName);
+                fileNameLabel.Text = fn;
+            }
+            else
+            {
+                webBrowser1.Dispose();
+                MoveFile();
+                MessageBox.Show(this, "Everything checked", "OK");
+            }
+        }
+        
+    }
+}
